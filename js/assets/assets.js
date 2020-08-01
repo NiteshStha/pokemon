ed1b0ddf0bb23051092cdf3a644effbb4bed2b66
Nitesh Shrestha <nitesh.shresthax@gmail.com>
2020-08-01 19:13:29 +0545

    Refactored the Image urls relative path

--- js/assets/assets.js
+++ js/assets/assets.js
@@ -2,7 +2,7 @@
 let IMAGE_COLLECTION;
 
 const IMAGE_URLS = {
-  BACKGROUND: '../../assets/images/background/battle-background.png',
-  DRAGONITE_FRONT: '../../assets/images/pokemon/dragonite-front.png',
-  DRAGONITE_BACK: '../../assets/images/pokemon/dragonite-back.png',
+  BACKGROUND: './assets/images/background/battle-background.png',
+  DRAGONITE_FRONT: './assets/images/pokemon/dragonite-front.png',
+  DRAGONITE_BACK: './assets/images/pokemon/dragonite-back.png',
 };
